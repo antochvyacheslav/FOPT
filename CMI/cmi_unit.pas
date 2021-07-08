aa17e2dbe176757d087a5ebf28cfdd9c19b4c108
antochvyacheslav <85184536+antochvyacheslav@users.noreply.github.com>
2021-07-08 13:53:19 +0300

    add files

---
+++ CMI/cmi_unit.pas
@@ -0,0 +1,106 @@
+unit cmi_unit;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, Grids, ExtCtrls;
+
+type
+  TCMI = class(TForm)
+    Memo1: TMemo;
+    sg1: TStringGrid;
+    Button1: TButton;
+    Image1: TImage;
+    Button2: TButton;
+    procedure FormActivate(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure risunok();
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  CMI: TCMI;
+  i,kol:integer;
+
+implementation
+
+{$R *.dfm}
+
+procedure TCMI.FormActivate(Sender: TObject);
+begin
+memo1.lines.LoadFromFile('text_files\cmi.txt');
+kol:=sg1.ColCount-1;
+button1.Click;
+end;
+
+procedure TCMI.Button1Click(Sender: TObject);
+var prov:boolean;//проверка, какое значеие имел прдыдущий символ 1: 11 или 00
+begin
+randomize;
+prov:=true;
+for i:=0 to kol do sg1.cells[i,0]:=inttostr(random(2));
+for i:=0 to kol do
+  if sg1.cells[i,0]='0' then sg1.cells[i,1]:='01'
+  else
+     if prov then
+     begin sg1.cells[i,1]:='11';prov:=false; end
+     else
+     begin sg1.cells[i,1]:='00';prov:=true;end;
+risunok();
+end;
+
+procedure Tcmi.Risunok();
+var x,h,i:integer;
+begin
+image1.picture:=nil;
+h:=19;
+x:=0;
+image1.Canvas.MoveTo(0,19);image1.Canvas.LineTo(495,19);
+for i:=0 to kol do
+  begin
+  if(sg1.cells[i,1]='11')then
+    begin
+    //вертик сверху слева
+    image1.Canvas.MoveTo(x,0);image1.Canvas.LineTo(x,19);
+    //вертик сверху по центру
+    image1.Canvas.MoveTo(x+(h div 2),0);
+    image1.Canvas.LineTo(x+(h div 2),19);
+    //вертик сверху справа
+    image1.Canvas.MoveTo(x+h,0);
+    image1.Canvas.LineTo(x+h,19);
+    //крышка
+    image1.Canvas.MoveTo(x,0);
+    image1.Canvas.LineTo(x+h+1,0);
+    x:=x+h;
+    end;
+  if sg1.Cells[i,1]='00' then
+    begin
+    image1.Canvas.MoveTo(x,19);image1.Canvas.LineTo(x+h+1,19);
+    x:=x+h;
+    end;
+  if(sg1.cells[i,1]='01')then
+    begin
+    //вертик сверху справа
+    image1.Canvas.MoveTo(x+(h div 2),0);
+    image1.Canvas.LineTo(x+(h div 2),19);
+    //вертик сверху слева
+    image1.Canvas.MoveTo(x+h,0);image1.Canvas.LineTo(x+h,19);
+    //крышка
+    image1.Canvas.MoveTo(x+(h div 2),0);
+    image1.Canvas.LineTo(x+h+1,0);
+    x:=x+h;
+    end;
+  end;
+end;
+
+procedure TCMI.Button2Click(Sender: TObject);
+begin
+CMI.close;
+end;
+
+end.
